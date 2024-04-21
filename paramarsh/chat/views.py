37edb80442bc129c7db9d2ef28f16ca9d98a8c78
Abhishek Verma <abhishekverma0313@gmail.com>
2024-04-21 23:00:51 +0530

    setup chat bubble room

--- paramarsh/chat/views.py
+++ paramarsh/chat/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+from .models import Room
+
+
+@require_POST
+def create_room(request,uuid):
+    name = request.POST.get('name','')
+    url = request.POST.get('url','')
+
+    Room.objects.create(uuid=uuid,student=name,url=url)
+
+    return JsonResponse({'message':'room created'})
+
--- paramarsh/static/js/main.js
+++ paramarsh/static/js/main.js
@@ -20,6 +20,71 @@ const chatOpenElement = document.querySelector('#chat_open')
 const chatWelcomeElement = document.querySelector('#chat_welcome')
 const chatJoinElement = document.querySelector('#chat_join')
 const chatRoomElement = document.querySelector('#chat_room')
+const chatNameElement = document.querySelector('#chat_name')
+const chatLogElement = document.querySelector('#chat_log')
+const chatInputElement = document.querySelector('#chat_message_input')
+const chatSubmitElement = document.querySelector('#chat_message_submit')
+
+
+
+/**
+ * Functions
+ 
+ */
+
+ function getCookie(name)
+ {
+   var cookieValue = null
+
+   if(document.cookie && document.cookie !='')
+   {
+    var cookies = document.cookie.split(';')
+
+    for(var i=0;i<cookies.length;i++)
+    {
+        var cookie=cookies[i].trim()
+        
+        if(cookie.substring(0,name.length+1) === (name + '='))
+        {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+
+            break
+        }
+    }
+   }
+   return cookieValue
+ }
+
+
+function JoinChatRoom(){
+
+    console.log('Join Chat Room')
+    chatName= chatNameElement.value
+
+    console.log('Join as :',chatName)
+    console.log('Room uuid:',chatRoomUuid)
+
+    const data=new FormData()
+    data.append('name',chatName)
+    data.append('url',chatWindowurl)
+
+    fetch('/api/create-room/${chatRoomUuid}/',{
+        method: 'POST',
+        headers : {
+            'X-CSRF Token':getCookie('csrftoken')
+        },
+        body : data
+    })
+
+    .then(function(res){
+        return res.json()
+    })
+
+    .then(function(data){
+        console.log('data',data)
+    }) 
+
+}
 
 
 /**
@@ -41,5 +106,8 @@ chatJoinElement.onclick = function(e){
     chatWelcomeElement.classList.add('hidden')
     chatRoomElement.classList.remove('hidden')
 
+    JoinChatRoom()
+
     return false
 }
+
