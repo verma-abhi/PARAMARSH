de96e9f151b44077cfe229e17a0174d43c622483
Abhishek Verma <abhishekverma0313@gmail.com>
2024-04-21 21:35:24 +0530

    javascript to navigate in the bubble

--- paramarsh/static/js/main.js
+++ paramarsh/static/js/main.js
@@ -1,1 +1,45 @@
 
+/**
+ * Variables
+ */
+
+let chatName = ''
+let chatSocket = null
+let chatWindowurl = window.location.href
+let chatRoomUuid = Math.random().toString(36).slice(2,12)
+
+
+
+/**
+ * Elements
+ */
+
+const chatElement = document.querySelector('#chat')
+const chatIconElement = document.querySelector('#chat_icon')
+const chatOpenElement = document.querySelector('#chat_open')
+const chatWelcomeElement = document.querySelector('#chat_welcome')
+const chatJoinElement = document.querySelector('#chat_join')
+const chatRoomElement = document.querySelector('#chat_room')
+
+
+/**
+ * Event Listeners
+ */
+
+chatOpenElement.onclick = function(e){
+    e.preventDefault()
+
+    chatIconElement.classList.add('hidden')
+    chatWelcomeElement.classList.remove('hidden')
+
+    return false
+}
+
+chatJoinElement.onclick = function(e){
+    e.preventDefault()
+
+    chatWelcomeElement.classList.add('hidden')
+    chatRoomElement.classList.remove('hidden')
+
+    return false
+}
