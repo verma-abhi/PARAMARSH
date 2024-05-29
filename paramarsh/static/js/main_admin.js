6b2a10d1753d486355fe480c718db7fed525e9ba
Abhishek Verma <abhishekverma0313@gmail.com>
2024-05-30 00:15:37 +0530

    making possible to send message to other users

--- paramarsh/static/js/main_admin.js
+++ paramarsh/static/js/main_admin.js
@@ -16,6 +16,96 @@ const chatLogElement = document.querySelector('#chat_log')
 const chatInputElement = document.querySelector('#chat_message_input')
 const chatSubmitElement = document.querySelector('#chat_message_submit')
 
+
+/**
+ * Functions
+ */
+
+
+
+function scrollToBottom() {
+    chatLogElement.scrollTop = chatLogElement.scrollHeight
+}
+
+
+function sendMessage() {
+    chatSocket.send(JSON.stringify({
+        'type': 'message',
+        'message': chatInputElement.value,
+        'name': document.querySelector('#user_name').textContent.replaceAll('"', ''),
+        'mentor': document.querySelector('#user_id').textContent.replaceAll('"', ''),
+    }))
+
+    chatInputElement.value = ''
+}
+
+
+function onChatMessage(data) {
+    console.log('onChatMessage', data)
+
+    if (data.type == 'chat_message') {
+        let tmpInfo = document.querySelector('.tmp-info')
+
+        if (tmpInfo) {
+            tmpInfo.remove()
+        }
+        
+        if (!data.mentor) {
+            chatLogElement.innerHTML += `
+                <div class="flex w-full mt-2 space-x-3 max-w-md">
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+
+                    <div>
+                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+                        
+                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+                    </div>
+                </div>
+            `
+        } else {
+            chatLogElement.innerHTML += `
+                <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                    <div>
+                        <div class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">${data.message}</p>
+                        </div>
+                        
+                        <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
+                    </div>
+
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+                </div>
+            `
+        }
+    } else if (data.type == 'writing_active') {
+        if (!data.mentor) {
+            let tmpInfo = document.querySelector('.tmp-info')
+
+            if (tmpInfo) {
+                tmpInfo.remove()
+            }
+
+            chatLogElement.innerHTML += `
+                <div class="tmp-info flex w-full mt-2 space-x-3 max-w-md">
+                    <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+
+                    <div>
+                        <div class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                            <p class="text-sm">The client is typing...</p>
+                        </div>
+                    </div>
+                </div>
+            `
+        }
+    }
+
+    scrollToBottom()
+}
+
+
+
 /**
  * Web socket
  */
@@ -25,7 +115,7 @@ chatSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoom}/`)
 chatSocket.onmessage = function(e) {
     console.log('on message')
 
-    
+    onChatMessage(JSON.parse(e.data))
 }
 
 chatSocket.onopen = function(e) {
@@ -34,4 +124,17 @@ chatSocket.onopen = function(e) {
 
 chatSocket.onclose = function(e) {
     console.log('chat socket closed unexpectadly')
+}
+
+
+/**
+ * Event listeners
+ */
+
+chatSubmitElement.onclick = function(e) {
+    e.preventDefault()
+
+    sendMessage()
+
+    return false
 }
